02abc0789e320235471f5e28e796018d8d976b22
João Gerliandro <joaogerliandro@gmail.com>
2024-02-08 16:18:03 -0800

    feat - adding xml's and server connection

--- include/common.h
+++ include/common.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <regex>
+
+#include <windows.h>
+#include <winldap.h>
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,22 @@
 
 int main()
 {
-    std::cout << "Project Setup !" << std::endl;
+    try
+    {
+        LDAP *ldap = ldap_init("ldap:/localhost", LDAP_PORT);
 
+        if (ldap == nullptr) 
+        {
+            throw std::runtime_error("Failed to connect to LDAP Server.");
+        }
+
+        ldap_unbind(ldap);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    
     return EXIT_SUCCESS;
 }
